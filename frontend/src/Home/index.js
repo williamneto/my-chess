efba0ecbca9194b4b94b5226e97284cad51cfe02
william.neto <william.g.neto@gmail.com>
2022-06-02 11:26:35 -0300

    Iniciando frontend

---
+++ frontend/src/Home/index.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <h1>my-chess</h1>;
+  };
+  
+  export default Home;
+  
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from "./Home"
+import Layout from "./Layout"
 import reportWebVitals from './reportWebVitals';
+
+import {
+  BrowserRouter ,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
